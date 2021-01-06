4f52ffca8de5b6759c8df07f3886812db02735e9
Thunchakorn Suwanjiaramanee <thunchakorn@Thunchakorns-MacBook-Air.local>
2021-01-06 16:54:22 +0700

    fix bug

--- merge_split.py
+++ merge_split.py
@@ -6,25 +6,23 @@ parser = argparse.ArgumentParser(description='merge, train-test split or do both
 parser.add_argument('-m', '--mode', default='m+s',
                     help='whether "m" for merge, "s" for split or "m+s" for merge then split')
 parser.add_argument('-id', '--img_dir',
-                    help='if mode include merge, specify path to image directory')
+                    help='specify path to image directory')
 parser.add_argument('-ad', '--ann_dir',
-                    help='if mode include merge, specify path to annotation directory')
+                    help='specify path to annotation directory')
 parser.add_argument('-a', '--annotations', metavar='coco_annotations', type=str,
                     help='specify only if mode = "s"; path to COCO annotations file.')
 parser.add_argument('-tr', '--train', type=str,
-                    help='specify only if mode = "s" or "m+s"; training annotations name')
+                    help='specify only if mode = "s" or "m+s"; training annotations path')
 parser.add_argument('-te', '--test', type=str, 
-                    help='specify only if mode = "s" or "m+s"; testing annotations name')
+                    help='specify only if mode = "s" or "m+s"; testing annotations path')
 parser.add_argument('-s', dest='split', type=float, required=True,
                     help="A percentage of a split; a number in (0, 1)")              
 
 args = parser.parse_args()
 
 def main(args):
+    cas = COCO_Assistant(args.img_dir, args.ann_dir)
     if args.mode == "m+s" or args.mode == "m":
-        img_dir = args.img_dir
-        ann_dir = args.ann_dir
-        cas = COCO_Assistant(img_dir, ann_dir)
         cas.merge()
         if args.mode == "m+s":
             cas.splitter(cas.dst_ann, args.split, os.path.join(cas.res_dir, args.train), os.path.join(cas.res_dir, args.test))
